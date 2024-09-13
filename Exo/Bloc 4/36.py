3125fe2317132a3cb5bbfbc00f6d7f332e526318
elpulpo0 <chris.elpulpo@gmail.com>
2024-09-13 07:14:17 +0200

    day 4

---
+++ Exo/Bloc 4/36.py
@@ -0,0 +1,31 @@
+exo = 36
+exo_title = f"Exo n°{exo}"
+
+print(f'''
+{exo_title :=^40}
+
+Écrivez un programme qui demande à l'utilisateur de deviner un nombre secret (par exemple 42). 
+Le programme indique à l'utilisateur si le nombre à deviner est plus grand ou plus petit que sa 
+proposition et continue de demander un nombre tant que l'utilisateur ne trouve pas le nombre secret. 
+Une fois que l'utilisateur trouve le nombre secret, affichez un message de félicitations.
+
+''')
+
+secretCode = "666"
+
+def fct(code):
+    if code != secretCode:
+        phraseUser = fct(input("Try again:"))
+    else:
+        print("""
+Congrats, you found the code !!
+        """)
+
+phraseUser = input("Find the secret code:")
+
+fct(phraseUser)
+
+print(f'''
+
+{"Fin de l'exo" :=^40}
+''')
